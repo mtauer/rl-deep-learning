6e4660d37a5c3094c9bc3dc2ef03a330e1d48be8
Michael Tauer <michael.tauer@futurice.com>
2018-09-02 13:52:25 +0200

    Improve read/write of training data

--- pandemic-light/trainingData.js
+++ pandemic-light/trainingData.js
@@ -7,21 +7,21 @@ import { getIterationStats, printIterationStats } from './stats';
 const episodesFile = './pandemic-light/training-data/episodes.json';
 
 export function saveEpisode(episodeStats, trainingExamples) {
-  const episodes = JSON.parse(fs.readFileSync(episodesFile));
+  const episodes = readEpisodes();
   episodes.push({
     episodeStats,
     trainingExamples,
   });
-  fs.writeFileSync(episodesFile, JSON.stringify(episodes));
+  writeEpisodes(episodes);
 }
 
 export function getSavedEpisodesCount() {
-  const episodes = JSON.parse(fs.readFileSync(episodesFile));
+  const episodes = fs.readFileSync(episodesFile) || [];
   return episodes.length;
 }
 
 export function summarizeSavedEpisodes() {
-  const episodes = JSON.parse(fs.readFileSync(episodesFile));
+  const episodes = readEpisodes();
   const episodesStats = episodes.map(e => last(e.episodeStats) || e.episodeStats);
   printIterationStats(getIterationStats(episodesStats));
   const wonEpisodesStats = episodesStats.filter(stats => stats.won);
@@ -29,7 +29,15 @@ export function summarizeSavedEpisodes() {
 }
 
 export function getSavedTrainingExamples() {
-  const episodes = JSON.parse(fs.readFileSync(episodesFile));
+  const episodes = readEpisodes();
   const trainingExamples = episodes.map(e => e.trainingExamples);
   return flatten(trainingExamples);
+}
+
+function readEpisodes() {
+  return JSON.parse(fs.readFileSync(episodesFile) || []);
+}
+
+function writeEpisodes(episodes) {
+  fs.writeFileSync(episodesFile, JSON.stringify(episodes));
 }
