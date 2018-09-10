7926c426f1448b6804ce1e000be77c7cdeac4f31
Michael Tauer <michael.tauer@futurice.com>
2018-09-10 20:12:25 +0200

    Add method to read training episodes

--- pandemic-light/storage.js
+++ pandemic-light/storage.js
@@ -6,8 +6,7 @@ import packageJson from '../package.json';
 
 // eslint-disable-next-line import/prefer-default-export
 export function writeTrainingEpisode(episodeStats, episodeResults, iteration) {
-  const iterationString = padStart(iteration, 3, '0');
-  const directory = `pandemic-light/v${packageJson.version}_iteration_${iterationString}_training_data`;
+  const directory = getTrainingDataDirectory(iteration);
   if (!fs.existsSync(directory)) {
     fs.mkdirSync(directory);
   }
@@ -16,4 +15,22 @@ export function writeTrainingEpisode(episodeStats, episodeResults, iteration) {
     episodeStats,
     episodeResults,
   }));
+}
+
+export function readTrainingEpisodes(iteration) {
+  const directory = getTrainingDataDirectory(iteration);
+  const episodes = [];
+  if (fs.existsSync(directory)) {
+    fs.readdirSync(directory).forEach((episodeFilename) => {
+      const episode = JSON.parse(fs.readFileSync(`${directory}/${episodeFilename}`));
+      episodes.push(episode);
+    });
+  }
+  return episodes;
+}
+
+function getTrainingDataDirectory(iteration) {
+  const iterationString = padStart(iteration, 3, '0');
+  const directory = `pandemic-light/v${packageJson.version}_iteration_${iterationString}_training_data`;
+  return directory;
 }
