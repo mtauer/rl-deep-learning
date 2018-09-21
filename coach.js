2a08b00df9920c5e067ec6359cfa79ae522cb24d
Michael Tauer <michael.tauer@futurice.com>
2018-09-21 19:58:20 +0200

    Train new models with the last 1500 training episodes

--- coach.js
+++ coach.js
@@ -69,7 +69,7 @@ export default class Coach {
     this.neuralNetwork = this.neuralNetwork || await this.getNeuralNetwork(iteration, version);
     console.log('Preparing training data');
     const trainingEpisodes = await this.trainingEpisodesStorage
-      .readTrainingEpisodes(iteration, version);
+      .readLastTrainingEpisodes(1500, version);
     const trainingExamples = shuffle(
       flatten(
         trainingEpisodes.map((trainingEpisode) => {
