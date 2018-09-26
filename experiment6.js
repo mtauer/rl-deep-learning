6f52a325147594f26176e4641dbefb4e1f8b34f8
Michael Tauer <michael.tauer@futurice.com>
2018-09-26 21:50:07 +0200

    Save model layers as json files

--- experiment6.js
+++ experiment6.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 import GoogleCloudStorage from './pandemic-light/googleCloudStorage';
 import PandemicNeuronalNetwork from './pandemic-light/neuralNetwork';
 
@@ -6,9 +8,10 @@ const googleCloudStorage = new GoogleCloudStorage();
 async function e6(version, iteration) {
   const neuralNetwork = new PandemicNeuronalNetwork();
   await googleCloudStorage.readModel(neuralNetwork, iteration, version);
-  const weights = neuralNetwork.pModel.getWeights();
-  console.log('weights', weights);
-  // console.log('weights', weights[1].dataSync());
+  const weights = neuralNetwork.pModel.layers[3].getWeights()
+    .map(w => ({ ...w, data: Array.from(w.dataSync()) }));
+
+  fs.writeFileSync('weights_0_3_2__7__layer_3.json', JSON.stringify(weights));
 }
 
 e6('0.3.2', 7);
