90742b24a374681d1c0e47d40b6d89eaa2d2ce08
Michael Tauer <michael.tauer@futurice.com>
2018-09-26 21:50:24 +0200

    Create empty neural network page

---
+++ pandemic-web/src/neuralNetwork/NeuralNetworkPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from '../components/Page';
+
+const NeuralNetworkPage = () => (
+  <Container>
+    <Title>Neural Network</Title>
+  </Container>
+);
+
+export default NeuralNetworkPage;
