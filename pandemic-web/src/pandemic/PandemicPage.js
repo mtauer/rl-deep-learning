32840ab37c7f411b43a6b69273ca74111c07e3e1
Michael Tauer <michael.tauer@futurice.com>
2018-09-30 13:53:10 +0200

    Clear UI for new web app

--- pandemic-web/src/pandemic/PandemicPage.js
+++ pandemic-web/src/pandemic/PandemicPage.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 import { Container, Title } from '../components/Page';
-import ValidActionsSection from './ValidActionsSection';
-import PlayersSection from './PlayersSection';
 
 const PandemicPage = () => (
   <Container>
     <Title>Deep reinforcement learning for Pandemic</Title>
-    <ValidActionsSection />
-    <PlayersSection />
   </Container>
 );
 
