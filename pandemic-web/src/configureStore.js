eec2e2491795914fdcaad60540eb425a30f33f93
Michael Tauer <michael.tauer@futurice.com>
2018-09-30 14:45:12 +0200

    Enable Redux devtools

--- pandemic-web/src/configureStore.js
+++ pandemic-web/src/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { createEpicMiddleware, combineEpics } from 'redux-observable';
 
 import pandemicReducer from './pandemic/redux';
@@ -6,14 +6,27 @@ import pandemicReducer from './pandemic/redux';
 const rootReducer = combineReducers({
   pandemic: pandemicReducer,
 });
+const rootEpic = combineEpics(
+);
+
 const epicMiddleware = createEpicMiddleware();
+const middleware = [epicMiddleware];
+const enhancers = [];
+
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line no-underscore-dangle
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(epicMiddleware),
+  composedEnhancers,
 );
-
-epicMiddleware.run(combineEpics(
-));
+epicMiddleware.run(rootEpic);
 
 export default store;
