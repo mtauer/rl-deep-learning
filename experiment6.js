9f375684e92798c314c48f76a3145ad42db0df67
Michael Tauer <michael.tauer@futurice.com>
2018-10-18 21:43:55 +0200

    Add model weights data

--- experiment6.js
+++ experiment6.js
@@ -8,10 +8,10 @@ const googleCloudStorage = new GoogleCloudStorage();
 async function e6(version, iteration) {
   const neuralNetwork = new PandemicNeuronalNetwork();
   await googleCloudStorage.readModel(neuralNetwork, iteration, version);
-  const weights = neuralNetwork.pModel.layers[0].getWeights()
+  const weights = neuralNetwork.pModel.layers[2].getWeights()
     .map(w => ({ ...w, data: Array.from(w.dataSync()) }));
 
-  fs.writeFileSync('pandemic-web/src/neuralNetwork/weights_0_3_2__18__layer_0.json', JSON.stringify(weights));
+  fs.writeFileSync(`pandemic-web/src/neuralNetwork/weights_${version}__${iteration}__layer_2.json`, JSON.stringify(weights));
 }
 
-e6('0.3.2', 18);
+e6('0.4.0', 1);
